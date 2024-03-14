9ad47c46eb97b6ea30d0b8dfdaecdc7b0646dd13
João <jomaatheus@gmail.com>
2024-03-13 22:04:12 -0300

    dsgasgd

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,33 +1,24 @@
 <?php
-  //Variáveis
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $telefone = $_POST['telefone'];
-  $mensagem = $_POST['mensagem'];
-  $data_envio = date('d/m/Y');
-  $hora_envio = date('H:i:s');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recolher os dados do formulário
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $descricao = $_POST['descricao'];
 
-  //Compo E-mail
-  $arquivo = "
-    <html>
-      <p><b>Nome: </b>$nome</p>
-      <p><b>E-mail: </b>$email</p>
-      <p><b>Mensagem: </b>$mensagem</p>
-      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-    </html>
-  ";
-  
-  //Emails para quem será enviado o formulário
-  $destino = "edipojoseoliveira@gmail.com";
-  $assunto = "Contato pelo Site";
+    // Configurar os detalhes do e-mail
+    $destinatario = "jomaatheus@gmail.com";
+    $assunto = "Novo formulário enviado";
+    $mensagem = "Nome: $nome\n";
+    $mensagem .= "E-mail: $email\n";
+    $mensagem .= "Telefone: $telefone\n";
+    $mensagem .= "Descrição: $descricao\n";
 
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
+    // Enviar o e-mail
+    mail($destinatario, $assunto, $mensagem);
 
-  //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
-  
-  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+    // Redirecionar de volta para a página do formulário
+    header("Location: index.html");
+    exit;
+}
 ?>
